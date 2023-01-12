b7b43f818980255c3716b1dc6dc63646dfa006e5
sarika <sarikanimbalkar97@gmail.com>
2023-01-12 18:30:06 +0530

    Player RollDie Option

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -6,8 +6,29 @@ public class SnakeLadder {
         int dice=((int)Math.random()*10)%6+1;
         return dice;
     }
+    static int getOption()
+    {
+        int opetion=((int)Math.random()*10)%3;
+        return opetion;
+    }
     public static void main(String[] args) {
         int dice=rollDie();
         System.out.println(dice);
+        int option=getOption();
+        System.out.println(option);
+        switch (option)
+        {
+            case 0:
+                player1Position-=dice;
+                System.out.println("SNAKE");
+                break;
+            case 1:
+                player1Position+=dice;
+                System.out.println("LADDER");
+                break;
+            case 2:
+                System.out.println("No Play");
+                break;
+        }
     }
 }
