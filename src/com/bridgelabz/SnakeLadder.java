ec18b47bb14665c97a489598239a3a552294ef24
sarika <sarikanimbalkar97@gmail.com>
2023-01-12 18:17:32 +0530

    Player Position

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
-
 public class SnakeLadder {
+    static int player1Position=0;
     public static void main(String[] args) {
         System.out.println("Welcome To Snake and Ladder Game...");
     }
