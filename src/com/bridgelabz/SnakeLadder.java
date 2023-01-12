4b86882cd0999c1824daca3f321311cdb7560667
sarika <sarikanimbalkar97@gmail.com>
2023-01-12 18:24:55 +0530

    Player RollDie

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -3,7 +3,7 @@ public class SnakeLadder {
     static int player1Position=0;
     static int rollDie()
     {
-        int dice=((int)Math.random()*10)+1;
+        int dice=((int)Math.random()*10)%6+1;
         return dice;
     }
     public static void main(String[] args) {
